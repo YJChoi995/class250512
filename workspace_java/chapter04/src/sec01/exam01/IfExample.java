737f1aa123fad19a748e113b20dc65a5e2c6a720
최연지 <duswl4759@gmail.com>
2025-05-29 17:31:47 +0900

    250529(목) 수업 끝

--- workspace_java/chapter04/src/sec01/exam01/IfExample.java
+++ workspace_java/chapter04/src/sec01/exam01/IfExample.java
@@ -92,5 +92,122 @@ public class IfExample {
 			System.out.println("로또 번호: " + rotto);
 		}
 		
+		System.out.println();
+		
+		
+		// switch문
+		int n1 = (int)(Math.random() * 6) +1;
+		
+		switch(n1) {
+		case 1 :
+			System.out.println("1번");
+			break;
+		case 2 :
+			System.out.println("2번");
+			break;
+		case 3 :
+			System.out.println("3번");
+			break;
+		case 4 :
+			System.out.println("4번");
+			break;
+		case 5 :
+			System.out.println("5번");
+			break;
+		default :
+			System.out.println("6번");
+			break;
+		}
+		
+		int n2 = (int)(Math.random() * 6) +1;
+		if(n2 == 1) {
+			System.out.println("num은 1입니다.");
+		} else if(n2 == 2 ) {
+			System.out.println("num은 2입니다.");
+		} else {
+			System.out.println("num은 1, 2가 아닙니다.");
+		}
+		
+		switch(n2) {
+		case 1:
+			System.out.println("num은 1입니다.");
+			break;
+		case 2:
+			System.out.println("num은 2입니다.");	
+			break;
+		default:
+			System.out.println("num은 1, 2가 아닙니다.");
+			break;
+		}
+		
+		// 문제
+		int month = (int)(Math.random() * 12) +1;
+		if (month >= 3 && month <= 5) {
+			System.out.println(month + "월 " + "계절: 봄");
+		} else if(month >= 6 && month <= 8){
+			System.out.println(month + "월 " + "계절: 여름");
+		} else if(month >= 9 && month <= 11){
+			System.out.println(month + "월 " + "계절: 가을");
+		} else {
+			System.out.println(month + "월 " + "계절: 겨울");
+		}
+		
+		switch (month) {
+		case 3: 
+		case 4: 
+		case 5: 
+			System.out.println(month + "월 " + "계절: 봄");
+			break;
+		case 6: 
+		case 7: 
+		case 8: 
+			System.out.println(month + "월 " + "계절: 여름");
+			break;
+		case 9: 
+		case 10: 
+		case 11: 
+			System.out.println(month + "월 " + "계절: 가을");
+			break;
+		default:
+			System.out.println(month + "월 " + "계절: 겨울");
+			break;
+		}
+		
+		
+		// 문제 풀기
+		// Q1. int 변수에 임의의 수를 넣고 그 수가 "양수", "0", "음수" 구분하여 출력
+		int abc = (int)(Math.random() * -100) + 1;
+	
+		if (abc == 0) { 
+			System.out.println("랜덤한 숫자: " + abc);
+		} else if (abc < 0) {
+			System.out.println("랜덤한 숫자는: " + abc+ ", 음수");
+		} else {
+			System.out.println("랜덤한 숫자는: " + abc+ ", 양수");
+		}
+		
+		// Q2. a와 b 두 수 중에서 큰 값을 출력
+		
+		// Q3. 시간과 분이 있을 때 35분이 지난 시간을 출력
+		
+		// Q4. 어떤 수가 15와 20 사이(포함)에 있는가?
+		
+		// Q5. 통장 잔액이 15000원 있을 때 출금액에 따라 
+		// "잔액이 부족합니다.", "얼마 출금했고 얼마 남았습니다.", "정확히 입력해주세요."
+		
+		// Q6. 입력한 값에 따라 
+		// (ex. 125) "100보다 큰 수이며, 양수이고, 홀수입니다"
+		// (ex. -6) "100보다 작은 수이며, 음수이고, 짝수입니다."
+		
+		// Q7. 어제 온도(2), 오늘 온도(영하 3도) 변수 두 개
+		// "오늘 온도는 영하 3도 입니다. 어제보다 5도 낮습니다." 출력
+		
+		// Q8. 변수에 두자리 숫자(10~99)가 있을 때
+		// 10의 자리와 1의 자리가 같은 수인지 판단
+		
+		// Q9. 사각형의 한쪽 모서리 x1:10, y1:20
+		// 대각선 모서리            x2:60, y2:100
+		// 이럴 때 새로운 점 x3, y3는 사각형에 포함되는가?
+		
     }
 }
