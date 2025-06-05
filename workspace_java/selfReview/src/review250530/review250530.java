92d8186a4423fccf97c8a04fe3865b0534d84054
최연지 <duswl4759@gmail.com>
2025-06-05 19:56:23 +0900

    no message

---
+++ workspace_java/selfReview/src/review250530/review250530.java
@@ -0,0 +1,16 @@
+package review250530;
+
+public class review250530 {
+	public static void main(String[] args) {
+		// 1. 구구단을 출력하시오
+
+		// 2. 구구단 2단을 출력하시오
+		
+		// 3. 2~10까지 짝수만 출력
+		
+		// 4. 10~1까지 거꾸로 출력
+		
+		
+		
+	}
+}
