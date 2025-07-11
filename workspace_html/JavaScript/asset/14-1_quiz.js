1639a367cf3d72eaf3e60a2fe32f6198717e3f8e
최연지 <duswl4759@gmail.com>
2025-07-11 17:29:04 +0900

    250711(금) 수업 끝. js 문제 풀기 중(어제부터)

--- workspace_html/JavaScript/asset/14-1_quiz.js
+++ workspace_html/JavaScript/asset/14-1_quiz.js
@@ -11,7 +11,6 @@ function bind() {
         orderResult1.innerHTML = '<strong>주문 정보</strong><br>이름 : ' + input1[0].value 
                                 + '<br>주소 : ' + input1[1].value
                                 + '<br>연락처 : ' + input1[2].value + '<br>';
-
     })
 
     // 문제 2. 배송정보의 '주문 정보와 같음'를 체크하면 주문정보의 input 값 가져오기
@@ -36,17 +35,118 @@ function bind() {
                                  + '<br>연락처 : ' + input2[2].value + '<br>';
     })
 
+    
+    /* 문제 3. 메뉴 선택시 특이하게 보이게 */
+    let menu = document.querySelectorAll('.menu');
 
+    for(let i=0; i<menu.length; i++){
+        menu[i].addEventListener('click', function(){
+            menu[i].style.color = 'blue';
+            menu[i].style.textDecoration = 'underline';
+        })
+
+        menu[i].addEventListener('mouseleave', function(){
+            menu[i].style.color = '';
+            menu[i].style.textDecoration = '';
+        })
+    }
+    
+
+    /* 문제 4. 이미지 크게 + 변경 */
+    let mini = document.querySelectorAll('.mini');
+    let imgView = document.querySelectorAll('.imgView');
+    let cnt = 0;
+
+    for(let i=0; i<mini.length; i++){
+        mini[i].addEventListener('mouseover', function(){
+            mini[i].style.border = 'lpx solid blue';
+            imgView[i].style.display = 'block'; 
+            cnt++;
+            imgView[i].style.zIndex = cnt;
+        })
+    }
 
 
     /* 문제 5. 피자 주문 시스템: 사이즈, 토핑, 도우 선택한 정보 한 번에 표시 */
+    // 주문 버튼 변수
+    let pizzaOrderbtn = document.querySelector('#pizzaOrderbtn')
     
     // 선택값 출력용 div의 변수
     let pizzaOrdered = document.querySelector('#pizzaOrdered');
+    
+    let size = document.querySelector('input[name="pizzaSize"]:checked');
+    let topping = document.querySelectorAll('.topping');
+    let dough = document.getElementsByName('dough');
+    
     // 값을 저장할 변수
     let save = '';
 
+    // '주문'버튼을 누르면 실행
+    pizzaOrderbtn.addEventListener('click', function(){
+        // 사이즈를 선택한 값을 표시
+        if(size){
+            console.log(size.value);
+            // 선택한 값들을 변수에 저장
+            save += '사이즈 : ' + size.value + '<br>';
+        }
+    
+        // 토핑이 check되면 console창에 값 표시
+        for(let i=0; i<topping.length; i++){
+            if(topping[i].checked == true){
+                console.log(topping[i].value);
+                // 선택한 값들을 변수에 저장
+                save += topping[i].value + ' / ';
+            }
+        }
 
-    /*  */
+        // 도우를 선택한 값을 표시
+        console.log(dough[0].value);
+        save += '<br>도우 종류 : ' + dough[0].value;
+        
+        // 주문 정보에 모든 값 표시
+        pizzaOrdered.innerHTML = save;
+    }) 
+    
+    
+    /* 문제 6.  */
+
+    // '추가' 버튼
+    const addbtn = document.querySelector('#addbtn');
+    // 작성한 '할 일' 
+    const todo = document.querySelector('#todo');
+    // 작성한 할 일 목록이 생성될 필드
+    const listField = document.querySelector('#listField');
+    
+    
+    // '추가'버튼을 누르면 작성한 할 일이 'listField'에 추가됨
+    addbtn.addEventListener('click', function(){
+        if(todo.value != ''){
+            // 체크박스 만들기
+            const workList = document.createElement('input');
+            workList.type = 'checkbox';
+            workList.value = todo.value;
+            workList.className = 'work';
+
+            // 라벨 만들기
+            const label = document.createElement('label');
+            label.appendChild(workList);                 // 체크박스 추가
+            
+            // 줄바꿈
+            const br = document.createElement('br');
+
+            // listField에 추가
+            listField.appendChild(label);
+            listField.appendChild(br);
+    
+            // listField.innerHTML += `<input type="checkbox" value=${todo.value} class="work"> ${todo.value}<br>` ;
+            
+            // '추가' 버튼을 누르면 입력칸을 초기화
+            todo.value = '';
+        }
+    })
+
+    
+
+    
 
 }
