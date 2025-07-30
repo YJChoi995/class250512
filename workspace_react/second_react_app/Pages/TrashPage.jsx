26340ce8ce8a814e3e9d1467700bcf758f816e23
최연지 <duswl4759@gmail.com>
2025-07-30 18:55:47 +0900

    250730(수) 리액트 수업 7일차

---
+++ workspace_react/second_react_app/Pages/TrashPage.jsx
@@ -0,0 +1,38 @@
+import { Container, IconButton, ListItemSecondaryAction, ListItemText } from "@mui/material";
+import { Paper, List, ListItem } from '@mui/material';
+import RestoreIcon from '@mui/icons-material/Restore';
+
+function TrashPage(props) {
+    // 삭제 아이템
+    // 복원 함수
+
+    const deletedItems = props.deletedItems; // 부모 컴포넌트에서 전달된 삭제된 아이템 목록
+    const restoreItem = props.restoreItem; // 부모 컴포넌트에서 전달된 복원 함수
+
+    return(
+        <Container maxWidth="md">
+            <h2>삭제된 아이템 목록</h2>
+            <Paper style={{ margin: 16, boxShadow: '0 5px 8px rgba(0,0,0,0.5)', width: '100%'}}>
+                <List>
+                    {deletedItems === 0 ? (
+                        <ListItem>
+                            <ListItemText primary = "삭제된 아이템이 없습니다" />
+                        </ListItem>
+                    ) : (
+                        deletedItems.map((item) => (
+                            <ListItem key = {item.id}>
+                                <ListItemText primary = {item.title} />
+                                    <ListItemSecondaryAction>
+                                        <IconButton onClick = {() => restoreItem(item)}>
+                                            <RestoreIcon />
+                                        </IconButton>    
+                                    </ListItemSecondaryAction>
+                            </ListItem>
+                        ))
+                    )}
+                </List>
+            </Paper>
+        </Container>
+    )
+}
+export default TrashPage;
--- workspace_react/second_react_app/src/App.jsx
+++ workspace_react/second_react_app/src/App.jsx
@@ -3,14 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Todo from './Components/Todo';
-import {Paper, List, Container} from '@mui/material';
+import {Paper, List, Container, Box, Button } from '@mui/material';
 import AddTodo from './Components/AddTodo';
+import TrashPage from '../Pages/TrashPage';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   const [items, setItems] = useState([]);
+  const [deletedItems, setDeletedItems] = useState([]);
 
   // item 상태를 업테이트 하는 함수
-  const addItems=(item)=>{
+  const addItems = (item) => {
     item.id="ID-" + Date.now(); // key를 위한 id
     item.done=false; // done 상태는 false로 초기화
 
@@ -20,7 +23,7 @@ function App() {
   }
 
   // 아이템 삭제 함수
-  const deleteItem=(item)=>{
+  const deleteItem = (item) => {
     // 삭제 전 확인 알림창 표시
     const confirmDelete = window.confirm("삭제하시겠습니까?");
 
@@ -30,8 +33,21 @@ function App() {
 
       const updateItems = [...newItems]; 
       setItems(updateItems); // 상태 업데이트
+      setDeletedItems([...deletedItems, item]); // 삭제된 아이템 저장
+      alert('삭제되었습니다');
       console.log("updateItems : ", updateItems);
     }
+  }
+
+  // 아이템 복원 함수
+  const restoreItem = (item) => {
+    // 삭제된 항목 목록에서 제거
+    const newDeletedItems = deletedItems.filter((e) => e.id !== item.id);
+    setDeletedItems(newDeletedItems);
+
+    // 원래 항복에 다시 추가
+    setItems([...items, item]);
+    alert('복원되었습니다');
   }
 
   const editItem = () => {
@@ -50,13 +66,32 @@ function App() {
   )
    
   return (
-    // <div className="App">{todoItems}</div>
-    <div className="App">
-      <Container maxWidth="md">
-        <AddTodo addItems={addItems}/>
-        <div>{todoItems}</div>
-      </Container>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Container maxWidth="md">
+          <Box sx={{display: 'flex', justifyContent: 'center', gap:2, mb:2, mt: 2}}>
+            <Button variant = "contained" component={Link} to="/">할 일 목록</Button>
+            <Button variant = "contained" component={Link} to="/trash">휴지통 {deletedItems.length}</Button>
+          </Box>
+          <Routes>
+            <Route
+              path="/" element={
+                <>
+                  <AddTodo addItems={addItems} />
+                  <div>{todoItems}</div>
+                </>
+              }
+            />
+            <Route
+              path="/trash" element={
+                <TrashPage deletedItems={deletedItems} restoreItem={restoreItem} />
+              }
+            />
+          </Routes>
+         
+        </Container>
+      </div>
+    </BrowserRouter>
   );
 }
 
