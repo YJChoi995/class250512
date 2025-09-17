fbd939abe656bfefa146c54e69e58196237a7f8a
최연지 <duswl4759@gmail.com>
2025-09-17 17:08:07 +0900

    250917(화) 수업 끝

---
+++ workspace_java/spring2_IoC_DI/src/main/java/kr/or/human2/controller/MemberController.java
@@ -0,0 +1,36 @@
+package kr.or.human2.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.or.human2.service.MemberService;
+
+@Controller
+public class MemberController {
+	
+	// 생성자
+	MemberController(){
+		System.out.println("MemberController 생성자 실행");
+	}
+	
+	
+	// 메소드
+	
+	@Autowired
+	MemberService memberService;
+	
+	@RequestMapping("/member")
+	String listMember() {
+		System.out.println("MemberController listMember() 실행");
+		
+//		MemberService memberService = new MemberService();
+		List list = memberService.getList();
+		System.out.println("list: " + list);
+		
+		return "home";
+	}
+
+}
---
+++ workspace_java/spring2_IoC_DI/src/main/java/kr/or/human2/service/MemberService.java
@@ -0,0 +1,45 @@
+package kr.or.human2.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import kr.or.human2.dao.MemberDAO;
+
+@Service
+public class MemberService {
+
+	// 1. 강한 의존 상태
+//	public List getList() {
+//		MemberDAO memberDAO = new MemberDAOImpl();
+//		
+//		return memberDAO.select();
+//	}
+	
+	// 2. 전달 받아서 의존성을 낮춘 상태(약하게 만듦)
+//	public List getList(MemberDAO memberDAO) {
+//		return memberDAO.select();
+//	}
+	
+//	private MemberDAO memberDAO = null;
+	
+	@Qualifier("dao1")
+	@Autowired
+	MemberDAO memberDAO;
+	
+	
+//	public void setMemberDAo(MemberDAO memberDAO) {
+//		this.memberDAO = memberDAO;
+//	}
+//	
+//	MemberService(MemberDAO memberDAO){
+//		this.memberDAO = memberDAO;
+//	}
+	
+	public List getList() {
+		return this.memberDAO.select();
+	}
+
+}
